cc5be81b1b6d9175555b4acd2e12f88d4959d9ca
borowicz <mateusz.borowicz@nokia.com>
2021-01-23 01:23:28 +0100

    Clearing

---
+++ src/app/models/post.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by borowicz on 23/01/21.
+ */
+export interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ServerJsonService} from '../server-json.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs/index';
 import {User} from '../models/user';
+import {Post} from '../models/post';
 
 @Component({
   selector: 'app-user',
@@ -11,17 +12,22 @@ import {User} from '../models/user';
 })
 export class UserComponent implements OnInit {
   private user$: Observable<User[]>;
-  private userPosts$: Observable<any[]>;
+  private userPosts$: Observable<Post[]>;
   selectedUser: string;
+  public radioData: any;
 
   constructor(private usersService: ServerJsonService,
-              private route: ActivatedRoute,
-              private router: Router) { }
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.selectedUser = this.route.snapshot.params.id;
     this.user$ = this.usersService.getUserId(this.selectedUser);
     this.userPosts$ = this.usersService.getUserPosts(this.selectedUser);
+    this.radioData = 'show';
+  }
+
+  isShowButtonSelected(): boolean {
+    return this.radioData === 'show';
   }
 
 }
